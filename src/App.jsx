72fc0b8308a799857d55daae46b841514772e8fc
mujeeb olagunju <maoltech27@gmail.com>
2022-09-03 18:37:03 +0100

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 import Cart from "./pages/Cart";
 
 
-
 const App = () => {
-  return <Cart />
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/productlist' element={<ProductList />} />
+      </Routes>
+    </Router>
+  )
 };
 
 export default App;
